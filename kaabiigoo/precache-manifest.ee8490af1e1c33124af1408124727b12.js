1f26d05188dd46c5ddb51fdc81794c4fd7748df0
huanhuanboy <weihuan.luositing@gmail.com>
2021-04-03 18:45:38 +0800

    update

--- kaabiigoo/precache-manifest.ee8490af1e1c33124af1408124727b12.js
+++ kaabiigoo/precache-manifest.ee8490af1e1c33124af1408124727b12.js
@@ -5,26 +5,26 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "fb06ed9faff0c867d143",
-    "url": "/kabigo/static/css/2.b26ad133.chunk.css"
+    "url": "/kaabiigoo/static/css/2.b26ad133.chunk.css"
   },
   {
     "revision": "8e8a0ea9556ded9056f6",
-    "url": "/kabigo/static/css/main.6de0f992.chunk.css"
+    "url": "/kaabiigoo/static/css/main.6de0f992.chunk.css"
   },
   {
     "revision": "fb06ed9faff0c867d143",
-    "url": "/kabigo/static/js/2.8499ffda.chunk.js"
+    "url": "/kaabiigoo/static/js/2.8499ffda.chunk.js"
   },
   {
     "revision": "5802ec95f87cfa61e347e14e323aae1d",
-    "url": "/kabigo/static/js/2.8499ffda.chunk.js.LICENSE.txt"
+    "url": "/kaabiigoo/static/js/2.8499ffda.chunk.js.LICENSE.txt"
   },
   {
     "revision": "8e8a0ea9556ded9056f6",
-    "url": "/kabigo/static/js/main.2cb2343a.chunk.js"
+    "url": "/kaabiigoo/static/js/main.2cb2343a.chunk.js"
   },
   {
     "revision": "81d7764a98017764f380",
-    "url": "/kabigo/static/js/runtime-main.69ba0b9b.js"
+    "url": "/kaabiigoo/static/js/runtime-main.69ba0b9b.js"
   }
 ]);
